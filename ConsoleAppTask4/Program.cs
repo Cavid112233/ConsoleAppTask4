934657f37ad76eb00d4fa63c43f6d12b648e9909
Cavid112233 <suleymanli535@gmail.com>
2023-03-12 07:52:24 +0400

    Add project files.

---
+++ ConsoleAppTask4/Program.cs
@@ -0,0 +1,495 @@
+﻿namespace ConsoleAppTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        #region T1
+        //Console.Write("eded daxil edin: ");
+        //int num = int.Parse(Console.ReadLine());
+
+        //if (num % 2 == 0)
+        //{
+        //    Console.WriteLine("{0} cut reqemdi", num);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("{0} tek reqemdi", num);
+        //}
+        #endregion
+        #region T2
+        //Console.Write("1-ci Ededi daxil edin: ");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.Write("2-ci ededi daxil edin: ");
+        //int num2 = int.Parse(Console.ReadLine());
+
+        //if (num1 % num2 == 0)
+        //{
+        //    int cavab = num1 / num2;
+        //    Console.WriteLine(cavab);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Bolunmur");
+        //}
+        #endregion
+        #region T3
+        //Console.Write("Enter a number: ");
+        //int num = int.Parse(Console.ReadLine());
+        //int digit = num / 10;
+        //int lastdigit = num - digit * 10;
+        //if (lastdigit == 7)
+        //{
+        //    Console.WriteLine("Beli");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Xeyr");
+        //}
+        #endregion
+        #region T4
+        //l1:
+        //    Console.Write("Eded daxil edin: ");
+        //    int num = int.Parse(Console.ReadLine());
+        //    if (num > 99)
+        //    {
+        //        goto l1;
+        //    }
+        //    int eded1 = num / 10;
+        //    int eded2 = num % 10;
+        //    if (eded1 == eded2)
+        //    {
+        //        Console.WriteLine("Beli");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Xeyr");
+        //    }
+        #endregion
+        #region T5
+        //l1:
+        //Console.Write("Enter a number: ");
+        //int num = int.Parse(Console.ReadLine());
+        //if (num > 99)
+        //{
+        //   goto l1;
+        //}
+        //int d1 = num / 10;
+        //int d2 = num % 10;
+        //int cem = d1 + d2;
+        //if (cem % 2 == 0)
+        //{
+        //int hasil = d1 * d2;
+        //   Console.WriteLine(hasil);
+        //}
+        #endregion
+        #region T6
+        //l1:
+        //Console.Write("Enter a number: ");
+        //int num = int.Parse(Console.ReadLine());
+        //if (num < 100 || num > 999)
+        //{
+        //    goto l1;
+        //}
+        //else
+        //{
+        //    int d1 = num - (num / 10) * 10;
+        //    int d2 = (num - (num / 100) * 100 - d1) / 10;
+        //    int d3 = num / 100;
+        //    if (d1 + d3 == d2)
+        //    {
+        //        Console.WriteLine("Beli");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Xeyr");
+        //    }
+        //}
+        #endregion
+        #region T7
+        //l1:
+        //Console.Write("Enter a number: ");
+        //int num = int.Parse(Console.ReadLine());
+        //if (num < 100 || num > 999)
+        //{
+        //    goto l1;
+        //}
+        //else
+        //{
+        //    int d1 = num - (num / 10) * 10;
+        //    int d2 = (num - (num / 100) * 100 - d1) / 10;
+        //    int d3 = num / 100;
+        //    if (d1 + d2 + d3 == d3 * d3)
+        //    {
+        //        Console.WriteLine("Beli");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Xeyr");
+        //    }
+        //}
+        #endregion
+        #region T8
+        //int eded;
+        //l1:
+        //Console.WriteLine("eded daxil edin: ");
+        //if (!int.TryParse(Console.ReadLine(), out eded))
+        //{
+        //    goto l1;
+        //}
+        //switch (eded)
+        //{
+        //    case 1:
+        //        Console.WriteLine("1-ci gun");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("2-ci gun");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("3-cu gun");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine("4-cu gun");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine("5-ci gun");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("6-ci gun");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine("7-ci gun");
+        //        break;
+        //}
+        #endregion
+        #region T9
+        //    int eded;
+        //l1:
+        //    Console.WriteLine("eded daxil edin: ");
+        //    if (!int.TryParse(Console.ReadLine(), out eded))
+        //    {
+        //        goto l1;
+        //    }
+        //    switch (eded)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("Yanvar");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("Fevral");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("Mart");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("Aprel");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("May");
+        //            break;
+        //        case 6:
+        //            Console.WriteLine("Iyun");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("Iyul");
+        //            break;
+        //        case 8:
+        //            Console.WriteLine("Avqust");
+        //            break;
+        //        case 9:
+        //            Console.WriteLine("Sentyabr");
+        //            break;
+        //        case 10:
+        //            Console.WriteLine("Oktyabr");
+        //            break;
+        //        case 11:
+        //            Console.WriteLine("Noyabr");
+        //            break;
+        //        case 12:
+        //            Console.WriteLine("Dekabr");
+        //            break;
+        //}
+        #endregion
+        #region T10
+        //Console.Write("Enter a number: ");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number: ");
+        //int num2 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number: ");
+        //int num3 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number: ");
+        //int num4 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number: ");
+        //int num5 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number: ");
+        //int num6 = int.Parse(Console.ReadLine());
+        //int[] nums = new int[6];
+        //nums[0] = num1;
+        //nums[1] = num2;
+        //nums[2] = num3;
+        //nums[3] = num4;
+        //nums[4] = num5;
+        //nums[5] = num6;
+        //int max = num1;
+        //for (int i = 0; i < nums.Length; i++)
+        //{
+        //    if (max < nums[i])
+        //    {
+        //        max = nums[i];
+        //    }
+        //}
+        //Console.Write("En boyuk eded: ");
+        //Console.WriteLine(max);
+        #endregion
+        #region T11
+        //Console.WriteLine("eded daxil edin: ");
+        //int num = int.Parse(Console.ReadLine());
+        //int ilk3reqemincemi = (num / 100000) + ((num / 10000) % 10) + ((num / 1000) % 10);
+        //int son3reqemincemi = ((num / 100) % 10) + ((num / 10) % 10) + (num % 10);
+        //int orta1=(num/1000) % 10;
+        //int orta2=(num/100) % 10;
+
+        //if (ilk3reqemincemi == son3reqemincemi)
+        //{
+        //    int ilk2 = num / 10000;
+        //    int axrinci2 = num % 100;
+        //    int yapisdirilmis = (ilk2 * 100) + axrinci2+orta1*10+orta2;
+        //    Console.WriteLine($"{yapisdirilmis}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("ilk 3 reqemin cemi son 3 reqemin cemine beraber deyil.");
+        //}
+        #endregion
+        #region T12
+        //l1:
+        //Console.Write("Enter a 6-digit integer: ");
+        //string input = Console.ReadLine();
+
+        //if (input.Length != 6)
+        //{
+        //    goto l1;
+        //}
+
+        //string outputString = "8" + input + "8";
+        //int outputInt;
+
+        //if (!Int32.TryParse(outputString, out outputInt))
+        //{
+        //    Console.WriteLine("Error converting output to integer.");
+        //    return;
+        //}
+
+        //Console.WriteLine("Output: " + outputInt);
+        #endregion
+        #region T13
+        //    int a;
+        //l1:
+
+        //    Console.WriteLine("eded:");
+        //    if (!int.TryParse(Console.ReadLine(), out a))
+        //    {
+        //        goto l1;
+        //    }
+        //    if (a > 99999999 || a < 1000000)
+        //    {
+        //        goto l1;
+        //    }
+        //    int qaliq = 0;
+        //    int newnum = 0;
+        //    bool next = false;
+        //    while (a > 0)
+        //    {
+        //        qaliq = a % 10;
+        //        a /= 10;
+        //        newnum = newnum * 10 + qaliq;
+        //    }
+        //    a = newnum;
+
+        //    Console.WriteLine(newnum);
+        //    int i = 0;
+        //    if(newnum==a) 
+        //    {
+        //    newnum = 0;
+        //    while (a > 0)
+        //    {
+        //      qaliq = a % 10;
+        //       a /= 10;
+        //       newnum = newnum * 10 + qaliq;
+        //        if(qaliq==0)
+        //        {
+        //                i++;
+        //        }
+        //    }
+        //    }
+        //      Console.WriteLine($"0 sayisi:{i}");
+        #endregion
+        #region T14
+        //Console.Write("eded daxil edin: ");
+        //string input = Console.ReadLine();
+
+        //if (input.Length > 8)
+        //{
+        //    Console.WriteLine("eded cox boyukdur.");
+        //    return;
+        //}
+
+        //int number;
+        //if (!int.TryParse(input, out number))
+        //{
+        //    Console.WriteLine("sehv daxil etmisiz.");
+        //    return;
+        //}
+
+        //int netice = int.Parse("1" + input);
+        //Console.WriteLine($"Result: {netice}");
+        #endregion
+        #region T15
+        //l1:
+        //    Console.Write("Enter a number: ");
+        //    int num = int.Parse(Console.ReadLine());
+        //    if (num < 1 || num > 365)
+        //    {
+        //        goto l1;
+        //    }
+        //    if (num >= 1 && num <= 31)
+        //    {
+        //        Console.WriteLine("Yanvar");
+        //    }
+        //    else if (num >= 32 && num <= 59)
+        //    {
+        //        Console.WriteLine("Fevral");
+        //    }
+        //    else if (num >= 60 && num <= 90)
+        //    {
+        //        Console.WriteLine("Mart");
+        //    }
+        //    else if (num >= 91 && num <= 120)
+        //    {
+        //        Console.WriteLine("Aprel");
+        //    }
+        //    else if (num >= 121 && num <= 151)
+        //    {
+        //        Console.WriteLine("May");
+        //    }
+        //    else if (num >= 152 && num <= 181)
+        //    {
+        //        Console.WriteLine("Iyun");
+        //    }
+        //    else if (num >= 182 && num <= 212)
+        //    {
+        //        Console.WriteLine("Iyul");
+        //    }
+        //    else if (num >= 213 && num <= 243)
+        //    {
+        //        Console.WriteLine("Avqust");
+        //    }
+        //    else if (num >= 244 && num <= 273)
+        //    {
+        //        Console.WriteLine("Sentyabr");
+        //    }
+        //    else if (num >= 274 && num <= 304)
+        //    {
+        //        Console.WriteLine("Oktyabr");
+        //    }
+        //    else if (num >= 305 && num <= 334)
+        //    {
+        //        Console.WriteLine("Noyabr");
+        //    }
+        //    else if (num >= 335 && num <= 365)
+        //    {
+        //        Console.WriteLine("Dekabr");
+        //    }
+
+        //    //###############################
+
+        //    if (num >= 20 && num <= 49)
+        //    {
+        //        Console.WriteLine("Dolca burcu");
+        //    }
+        //    else if (num >= 50 && num <= 79)
+        //    {
+        //        Console.WriteLine("Baliqlar burcu");
+        //    }
+        //    else if (num >= 80 && num <= 109)
+        //    {
+        //        Console.WriteLine("Qoc burcu");
+        //    }
+        //    else if (num >= 110 && num <= 140)
+        //    {
+        //        Console.WriteLine("Buga burcu");
+        //    }
+        //    else if (num >= 141 && num <= 171)
+        //    {
+        //        Console.WriteLine("Ekizler burcu");
+        //    }
+        //    else if (num >= 172 && num <= 203)
+        //    {
+        //        Console.WriteLine("Xerceng burcu");
+        //    }
+        //    else if (num >= 204 && num <= 234)
+        //    {
+        //        Console.WriteLine("Sir burcu");
+        //    }
+        //    else if (num >= 235 && num <= 265)
+        //    {
+        //        Console.WriteLine("Qiz burcu");
+        //    }
+        //    else if (num >= 266 && num <= 295)
+        //    {
+        //        Console.WriteLine("Terezi burcu");
+        //    }
+        //    else if (num >= 296 && num <= 325)
+        //    {
+        //        Console.WriteLine("Eqreb burcu");
+        //    }
+        //    else if (num >= 326 && num <= 355)
+        //    {
+        //        Console.WriteLine("Oxatan burcu");
+        //    }
+        //    else if (num >= 356 || num <= 19)
+        //    {
+        //        Console.WriteLine("Oglaq burcu");
+        //    }
+        #endregion
+        #region T16
+        //l1:
+            //Console.WriteLine("6 Reqemli Ededinizi Daxil Edin: ");
+            //int eded;
+            //if (!int.TryParse(Console.ReadLine(), out eded))
+            //{
+            //    Console.WriteLine("Ededinizi Duzgun Daxil Etmediniiz!");
+            //    goto l1;
+            //}
+            //if (eded > 999999 || eded < 100000)
+            //{
+            //    Console.WriteLine("Edediniz 6 Reqemli Deyil!");
+            //    goto l1;
+
+            //}
+            //int qaliq, size;
+            //int[] reqemler = new int[0];
+            //while (eded > 0)
+            //{
+            //    qaliq = eded % 10;
+            //    eded /= 10;
+
+
+
+            //    if (Array.IndexOf(reqemler, qaliq) == -1)
+            //    {
+            //        size = reqemler.Length;
+            //        Array.Resize(ref reqemler, size + 1);
+            //        reqemler[size] = qaliq;
+            //    }
+            //}
+            //Console.WriteLine(reqemler.Length);
+            //foreach (var item in reqemler)
+            //{
+            //    Console.WriteLine(item);
+            
+        }
+        #endregion
+    }
+    
+}
